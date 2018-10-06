fdd44b3b41cae9f6cd79c3101d8a63997d37cd36
Dario Alejandro Soto <sotodario3@gmail.com>
2018-10-06 08:40:21 -0400

    add footer component into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { NgxGalleryModule } from 'ngx-gallery';
 import 'hammerjs';
 import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
