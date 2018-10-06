f3e10ed8e2d89f7773e88d8dd429f703f135ebe6
Dario Alejandro Soto <sotodario3@gmail.com>
2018-10-06 09:22:12 -0400

    add dependencies for backend mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NgxGalleryModule } from 'ngx-gallery';
-import 'hammerjs';
-import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpModule } from "@angular/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { NgxGalleryModule } from "ngx-gallery";
+import "hammerjs";
+import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from "./components/footer/footer.component";
+
+import { AuthenticationService } from "./services/authentication.service";
+import { fakeBackendProvider } from "./helpers/fake-backend";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    NgxGalleryModule
+    NgxGalleryModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService, fakeBackendProvider],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
