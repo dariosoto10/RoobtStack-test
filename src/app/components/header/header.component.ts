69c0fd750776ad9c96211a76ee84f466f3daa508
Dario Alejandro Soto <sotodario3@gmail.com>
2018-10-05 22:16:31 -0400

    ng lint

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,84 +10,59 @@ export class HeaderComponent implements OnInit {
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
 
-  
-
-  public imagesUrl;
-  imageObject: Array<object> = [{
-      image: './assets/tile.png',
-      thumbImage: './assets/tile.png'
-  }, {
-      image: './assets/tile.png',
-      thumbImage: './assets/tile.png'
-  }, {
-      image: './assets/tile.png',
-      thumbImage: './assets/tile.png'
-  },
-  ];
-
   constructor() { }
 
   ngOnInit() {
-    this.imagesUrl = [
-      './assets/tile.png',
-      './assets/tile-1.png',
-      './assets/overlay.png',
-      './assets/tile-3.png',
-      './assets/tile-4.png'
-      ];
-
-      this.galleryOptions = [
-        {
-            width: '600px',
-            height: '400px',
-            thumbnailsColumns: 4,
-            imageAnimation: NgxGalleryAnimation.Slide
-        },
-        // max-width 800
-        {
-            breakpoint: 800,
-            width: '100%',
-            height: '600px',
-            imagePercent: 80,
-            thumbnailsPercent: 20,
-            thumbnailsMargin: 20,
-            thumbnailMargin: 20
-        },
-        // max-width 400
-        {
-            breakpoint: 400,
-            preview: false
-        },
-
-        { "image": false, "height": "100px", "arrowPrevIcon": "fa fa-angle-left", "arrowNextIcon": "fa fa-angle-right", "imageAutoPlay": true,  "preview": false },
-        { "breakpoint": 500, "width": "100%" }
-    ];
+  this.galleryOptions = [
+      {
+        width: '600px',
+        height: '400px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      {
+        breakpoint: 400,
+        preview: false
+      },
+// tslint:disable-next-line:max-line-length
+    { 'image': false, 'height': '100px', 'arrowPrevIcon': 'fa fa-angle-left', 'arrowNextIcon': 'fa fa-angle-right', 'imageAutoPlay': true,  'preview': false },
+    { 'breakpoint': 500, 'width': '100%' }
+  ];
 
     this.galleryImages = [
-      {
-          small: './assets/tile.png',
-          medium: './assets/tile.png',
-          big: './assets/tile.png'
-      },
-      {
-          small: './assets/tile-1.png',
-          medium: './assets/tile-1.png',
-          big: './assets/tile-1.png'
-      },
-      {
-          small: './assets/overlay.png',
-          medium: './assets/overlay.png',
-          big: './assets/overlay.png'
-      },
-      {
-        small: './assets/tile-3.png',
-        medium: './assets/tile-3.png',
-        big: './assets/tile-3.png'
+    {
+      small: './assets/tile.png',
+      medium: './assets/tile.png',
+      big: './assets/tile.png'
     },
     {
-      small: './assets/tile-4.png',
-      medium: './assets/tile-4.png',
-      big: './assets/tile-4.png'
+      small: './assets/tile-1.png',
+      medium: './assets/tile-1.png',
+      big: './assets/tile-1.png'
+    },
+    {
+      small: './assets/overlay.png',
+      medium: './assets/overlay.png',
+      big: './assets/overlay.png'
+    },
+    {
+      small: './assets/tile-3.png',
+      medium: './assets/tile-3.png',
+      big: './assets/tile-3.png'
+    },
+    {
+    small: './assets/tile-4.png',
+    medium: './assets/tile-4.png',
+big: './assets/tile-4.png'
     },
     {
       small: './assets/tile.png',
